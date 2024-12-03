20d58d299c33251eac1850b465ed8365cc00be20
SYED <rehamans417@gmail.com>
2024-12-03 14:38:37 +0530

    defect-fixed-3245: Was returning null value as response in postman.

--- src/main/java/com/crm/controller/CommentController.java
+++ src/main/java/com/crm/controller/CommentController.java
@@ -19,6 +19,8 @@ public class CommentController {
 
     @PostMapping("/comments")
     public String createComment(@RequestBody Comment comment, @RequestParam long postId){
+        System.out.println(1000);
+        System.out.println(3000);
         Post post = postRepository.findById(postId).get();
         comment.setPost(post);
         commentRepository.save(comment);
